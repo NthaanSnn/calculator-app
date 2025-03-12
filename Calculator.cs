48ca2324f25d3a49196231772c2fabad620ead0b
NthaanSnn <alfarzani12@amikom.ac.id>
2025-03-12 14:11:48 +0700

    Fungsi Main

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,14 @@ class Calkukalor
 {
 	static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekamn Sembarang key untuk keluar");
+		Console.Readkey();
 	
 	}
 	
